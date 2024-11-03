73d268e4a191558a616eeedbff6dfc561850027f
12JC83 <gottajill@gmail.com>
2024-11-03 22:01:52 +0100

    fix

---
+++ Task3.1/Queue/Queue/QueueClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Queue<T> : IQueue<T>
+{
+    private LinkedList<T> elements = new LinkedList<T>();
+    public void Enqueue(T item)
+    {
+       elements.AddLast(item);
+       
+    }
+
+    public bool isEmpty()
+    {
+        return elements.Count == 0;
+    }
+
+    public T Dequeue()
+    {
+        if (isEmpty())
+            throw new InvalidOperationException("Queue is empty.");
+        T value = elements.First.Value;
+        elements.RemoveFirst();
+        return value;
+    }
+}
+
---
+++ Task3.1/Queue/Queue/QueueInterface.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+public interface IQueue<T>
+    {
+        void Enqueue(T item);
+        T Dequeue();
+        bool isEmpty();
+    }
+
